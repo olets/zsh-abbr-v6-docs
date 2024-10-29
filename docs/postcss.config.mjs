c09f1bfff7b227ce0154e82f9a61360b2da89421
Henry Bley-Vroman <olets@olets.dev>
2024-10-29 10:44:41 -1000

    refactor(tailwind): tighten up implementation, per my new blog post...
    https://www.olets.dev/posts/styling-vitepress-with-tailwind/

--- docs/postcss.config.mjs
+++ docs/postcss.config.mjs
@@ -1,7 +1,9 @@
-export default {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
-    "postcss-import": {},
     tailwindcss: {},
     autoprefixer: {},
   },
 };
+
+export default config;
